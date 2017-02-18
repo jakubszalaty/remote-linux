20e3c4ed0bd40ebb02e7784929c9e7ca3c97b26f
Kuba Szalaty <nnaisur@gmail.com>
2017-02-18 22:10:29 +0100

    Change spotifyInfo inverval time; Send spotiinfo on next prev

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 'use strict'
 
+const SPOTIFY_PLAYPAUSE = 'dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause'
+const SPOTIFY_NEXT = 'dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next'
+const SPOTIFY_PREVIOUS = 'dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous'
+const SPOTIFY_INFO = 'dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:\'org.mpris.MediaPlayer2.Player\' string:\'Metadata\''
+
 // const net = require('net')
 const http = require('http')
 const ip = require('ip')
@@ -12,15 +17,18 @@ const IP_ADDRESS = ip.address()
 
 const server = http.createServer()
 const io = require('socket.io')(server)
-io.on('connection', function(client){
 
-    console.log('client connected',client.id)
+io.on('connection', function (client) {
 
-    client.on('disconnect', ()=>{
-        console.log('client disconnected',client.id)
+    console.log('client connected', client.id)
+    let spotifyInterval = null
+
+    client.on('disconnect', () => {
+        console.log('client disconnected', client.id)
+        clearInterval(spotifyInterval)
     })
 
-    client.on('get_commands_list', ()=>{
+    client.on('get_commands_list', () => {
         console.log('get_commands_list')
         client.emit('commands_list', {
             data: [
@@ -38,16 +46,16 @@ io.on('connection', function(client){
         })
     })
 
-    client.on('mouseMoveCenter', () =>{
+    client.on('mouseMoveCenter', () => {
         const screenSize = robot.getScreenSize()
-        robot.moveMouse(screenSize.width/2, screenSize.height/2)
+        robot.moveMouse(screenSize.width / 2, screenSize.height / 2)
     })
 
-    client.on('mouseMoveZero', () =>{
-        robot.moveMouse(0,0)
+    client.on('mouseMoveZero', () => {
+        robot.moveMouse(0, 0)
     })
 
-    client.on('mouseMoveSin', () =>{
+    client.on('mouseMoveSin', () => {
         const twoPI = Math.PI * 2.0
         const screenSize = robot.getScreenSize()
         const height = (screenSize.height / 2) - 10
@@ -59,40 +67,89 @@ io.on('connection', function(client){
         }
     })
 
-    client.on('volumeup', ()=>{
-        shelljs.exec('amixer -D pulse sset Master 5%+ >> /dev/null')
+    client.on('volumeup', () => {
+        shelljs.exec('amixer -D pulse sset Master 5%+', { silent: true })
     })
 
-    client.on('volumedown', ()=>{
-        shelljs.exec('amixer -D pulse sset Master 5%- >> /dev/null')
+    client.on('volumedown', () => {
+        shelljs.exec('amixer -D pulse sset Master 5%-', { silent: true })
     })
 
-    client.on('spotify', ()=>{
-        shelljs.exec('spotify >> /dev/null 2>> /dev/null &')
+    client.on('spotify', () => {
+        spotifyInterval = setInterval(() => {
+            sendSpotifyInfo().then((data) => {
+                client.emit('spotifyInfo', parseDbusData(data))
+            })
+        }, 5000)
+
+        shelljs.exec('spotify&', { silent: true }, () => { })
     })
-    client.on('spotifyPlayPause', ()=>{
-        shelljs.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause >> /dev/null')
+    client.on('spotifyPlayPause', () => {
+
+        const cmd = shelljs.exec(SPOTIFY_PLAYPAUSE , { async: true, silent: true })
+        cmd.stdout.on('data', (data) => {
+            sendSpotifyInfo().then((data) => {
+                client.emit('spotifyInfo', parseDbusData(data))
+            })
+        })
+
     })
 
-    client.on('spotifyNext', ()=>{
-        shelljs.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next >> /dev/null')
+    client.on('spotifyNext', () => {
+
+        const cmd = shelljs.exec(SPOTIFY_NEXT, { async: true, silent: true })
+        cmd.stdout.on('data', (data) => {
+            sendSpotifyInfo().then((data) => {
+                client.emit('spotifyInfo', parseDbusData(data))
+            })
+        })
     })
 
-    client.on('spotifyPrevious', ()=>{
-        shelljs.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous >> /dev/null')
+    client.on('spotifyPrevious', () => {
+
+        const cmd = shelljs.exec(SPOTIFY_PREVIOUS, { async: true, silent: true })
+        cmd.stdout.on('data', (data) => {
+            sendSpotifyInfo().then((data) => {
+                client.emit('spotifyInfo', parseDbusData(data))
+            })
+        })
+
     })
 
-    client.on('spotifyClose', ()=>{
+    client.on('spotifyClose', () => {
         shelljs.exec('kill $(ps aux | grep spotify | awk \'{print $2}\')')
+        clearInterval(spotifyInterval)
     })
 
-    client.on('openUrl', (data)=>{
+    client.on('openUrl', (data) => {
         shelljs.exec(`google-chrome ${data.url}`)
     })
 
     // client.emit('test_connection', { data: 'witaj!' })
-
 })
 server.listen(1337, IP_ADDRESS)
 
 console.log(`Address: ${IP_ADDRESS}:1337`)
+
+
+function sendSpotifyInfo() {
+    return new Promise((resolve, reject) => {
+        const cmd = shelljs.exec(SPOTIFY_INFO, { async: true, silent: true })
+
+        cmd.stdout.on('data', (data) => { resolve(data) })
+    })
+}
+
+function parseDbusData(data) {
+    data = data.replace(/( )+/g, ' ')
+    data = data.replace(/\n/g, '')
+    data = data.replace(/dict entry\(/g, '\ndict entry(')
+
+    data = data.match(/dict entry\(.*\)/g).join('\n')
+    data = data.replace(/dict entry\( string /g, '')
+    data = data.replace(/ variant \w+ /g, ':')
+    data = data.replace(/ \)/g, '')
+    data = data.replace(/ string /g, '')
+
+    return JSON.parse(`{${data.replace(/\n/g, ',')}}`)
+}
